50a5b14ee3cf2cd25d6cc04f7fd1c1c4ab78eca2
Kanan E. Alizada <Kanan.Alizada@kapitalbank.az>
2019-07-30 22:05:21 +0400

    Exercise1 completed

---
+++ Part-1/src/Exercise1.java
@@ -0,0 +1,11 @@
+/**
+ * @author Kanan
+ */
+//Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+public class Exercise1 {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.print("Kanan Alizada");
+    }
+
+}
